60d88ed8dfdf308fe3a3c0c9f7494cf744bcc09e
liumenghui <liumenghui@genomics.cn>
2022-03-08 15:27:22 +0800

    长整数溢出demo

---
+++ VerifyUlongOverflow.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+ulong ss = ulong.MaxValue;
+ss = ss + 1;
+Console.WriteLine("Hello, World!");
